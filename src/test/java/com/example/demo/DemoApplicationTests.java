7a0b9b4810447234a66dd4a2060a29e924d106e6
zunggzing <hello.trungvinh@gmail.com>
2021-11-02 23:28:23 +0700

    Add Eureka Server Client

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+@EnableEurekaServer
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
